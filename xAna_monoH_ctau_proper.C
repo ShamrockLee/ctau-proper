3a12ef056f82cd0c89b62c08d70f5d8aa2a03ab5
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2019-12-24 01:10:55 +0800

    Rename ctau_lab and properly scale histograms Rename ctau_lab (from the original script) to betatau_lab to make it clear that it represents decay length. Generate the (true) ctau_lab and ctau_proper. Adjust the lower and upper limits and bin number of each histogram.

--- xAna_monoH_ctau_proper.C
+++ xAna_monoH_ctau_proper.C
@@ -22,11 +22,16 @@ void xAna_monoH_ctau(std::string inputFile,std::string outputFile, bool debug=fa
   Long64_t nTotal=0;
   Long64_t nPass[20]={0};
 
-  TH1F* h_ctau=new TH1F("h_ctau","",100,0,1);
-  TH1F* h_ctau2=new TH1F("h_ctau2","",100000,0,10000);
-  TH1F* h_ctau_lab  = (TH1F*)h_ctau2->Clone("h_ctau_lab");
-  TH1F* h_ctau_proper  = (TH1F*)h_ctau2->Clone("h_ctau_proper");
-  TH1F* h_chi2_gamma  = (TH1F*)h_ctau2->Clone("h_chi2_gamma");
+  //TH1F* h_ctau=new TH1F("h_ctau","",100,0,1);
+  //TH1F* h_ctau2=new TH1F("h_ctau2","",100000,0,10000);
+  //TH1F* h_ctau_lab  = (TH1F*)h_ctau2->Clone("h_ctau_lab");
+  //TH1F* h_chi2_gamma  = (TH1F*)h_ctau2->Clone("h_ctau_proper");
+  //TH1F* h_chi2_gamma  = (TH1F*)h_ctau2->Clone("h_chi2_gamma");
+  TH1F* h_betatau_lab =new TH1F("h_betatau_lab","",1000,0,500);
+  TH1F* h_ctau_lab =new TH1F("h_ctau_lab","", 1000, 0, 500);
+  TH1F* h_ctau_proper =new TH1F("h_ctau_proper","", 1000, 0, 2);
+  TH1F* h_chi2_beta =new TH1F("h_chi2_beta","", 1000, 0.9999, 1.0001);
+  TH1F* h_chi2_gamma =new TH1F("h_chi2_gamma","", 1000, 0, 10000);
 
   //Event loop
   for(Long64_t jEntry=0; jEntry<data.GetEntriesFast() ;jEntry++){
@@ -70,26 +75,47 @@ void xAna_monoH_ctau(std::string inputFile,std::string outputFile, bool debug=fa
 
       TVector3 dist =  *chi1_vtx - *chi2_vtx;
       
-      double ctau_lab = dist.Mag();
+      double betatau_lab = dist.Mag();
       double chi2_gamma = chi2_p4->Gamma();
-      double ctau_proper = ctau_lab / (double)(chi2_p4->Gamma());
+      double chi2_beta = chi2_p4->Beta();
+      double ctau_lab = betatau_lab / chi2_beta;
+      double ctau_proper = ctau_lab / chi2_gamma;
 
 
       if(debug){
 	cout << "ctau_lab = " << ctau_lab;
       }
       
+      h_betatau_lab -> Fill(betatau_lab);
       h_ctau_lab -> Fill(ctau_lab);
+      h_chi2_beta -> Fill(chi2_beta);
       h_chi2_gamma -> Fill(chi2_gamma);
       h_ctau_proper -> Fill(ctau_proper);
     }
   }
     
 
-
+  //Some shared library needed.
+  /*
+  Double_t ctauLabMaximum = h_betatau_lab->GetMaximum();
+  Int_t ctauLabMaximumBin = h_ctau_lab->GetMaximumBin();
+  Double_t ctauLabMinimum = h_ctau_lab->GetMinimum();
+  Int_t ctauLabMinimumBin = h_ctau_lab->GetMinimumBin();
+  Double_t ctauProperMaximum = h_ctau_proper->GetMaximum();
+  Int_t ctauProperMaximumBin = h_ctau_proper->GetMaximumBin();
+  Double_t ctauProperMinimum = h_ctau_proper->GetMinimum();
+  Int_t ctauProperMinimumBin = h_ctau_proper->GetMinimumBin();
+  Double_t chi2GammaMaximum = h_chi2_gamma->GetMaximum();
+  Int_t chi2GammaMaximumBin = h_chi2_gamma->GetMaximumBin();
+  Double_t chi2GammaMinimum = h_chi2_gamma->GetMinimum();
+  Int_t chi2GammaMinimumBin = h_chi2_gamma->GetMinimumBin();
+  */
+  
   TFile* outFile = new TFile(outputFile.data(),"recreate");
+  h_betatau_lab->Write();
   h_ctau_lab->Write();
   h_ctau_proper->Write();
+  h_chi2_beta->Write();
   h_chi2_gamma->Write();
   outFile->Close();
   std::cout << "nTotal    = " << nTotal << std::endl;
