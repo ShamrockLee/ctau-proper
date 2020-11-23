6b381ae909a210b885aff77b2a10bd96a0085763
Yueh-Shun Li <yuehshun@lxplus742.cern.ch>
2020-11-23 09:27:11 +0100

    Correct the format of dataTest_signal*.txt, submit, and merge (success)

--- lxplus_HTcondor/xAna_monoZ_genhist.C
+++ lxplus_HTcondor/xAna_monoZ_genhist.C
@@ -97,6 +97,9 @@ void xAna_monoZ_genhist(const TString nameCondorPack, const TString nameDatagrou
       vCrossSection = {
 #include "preselect/crossSections_DYJets_withComma.txt"
       };
+    } else if (nameDatagroup == "signal_Mx2-150_Mv-500_Mx1-1_ctau-1") {
+      vNumberFile = {4};
+      vCrossSection = {1};
     }
   }
 
