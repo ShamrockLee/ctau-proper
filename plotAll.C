6a3ecc4c041a2e028198c0b7b6f87d8791254870
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-02-25 16:08:57 +0800

    plotAll.C: Add parameters `normalize` and `logy`
    Add switch to make normalize and logy available
    
    Use `gRoot->MakeDefCanvas` to generate default TCanvas for gPad
    gSystem->mkdir(dirOut) before drawing and printing

--- plotAll.C
+++ plotAll.C
@@ -1,3 +1,4 @@
+#include<TROOT.h>
 #include<TSystem.h>
 #include<TDirectory.h>
 #include<TFile.h>
@@ -8,21 +9,26 @@
 #include<TH1.h>
 #include<TLegend.h>
 
-void plotAll(TString pathFileIn, TString dirOut);
+void plotAll(TString pathFileIn, TString dirOut, Bool_t normalize, Bool_t logy, Option_t *optionDraw);
 
-void plotAll(const char* pathFileIn, const char* dirout) {
-  plotAll((TString) pathFileIn, (TString) dirout);
+void plotAll(const char* pathFileIn, const char* dirout, Bool_t normalize = false, Bool_t logy = false, Option_t *optionDraw = "") {
+  plotAll((TString) pathFileIn, (TString) dirout, normalize, logy, optionDraw);
 }
 
-void plotAll(TString pathFileIn, TString dirOut) {
+void plotAll(TString pathFileIn, TString dirOut, Bool_t normalize = false, Bool_t logy = false, Option_t *optionDraw = "") {
   TString seperatorPath = "/";
   TFile *fileIn = TFile::Open(pathFileIn);
   TString basenameFileIn = gSystem->GetFromPipe(
       Form("file=%s; test=${file##*/}; echo \"${test%%.root}\"",
       pathFileIn.Data()));
+  gSystem->mkdir(dirOut);
   if (dirOut.Length() > 1 && dirOut.EndsWith(seperatorPath)) {
     dirOut.Resize(dirOut.Length()-1);
   }
+  if (!gPad) {
+    gROOT->MakeDefCanvas();
+  }
+  gPad->SetLogy(logy);
   for (auto&& keyRaw: *fileIn->GetListOfKeys()) {
     TKey *key = (TKey *) keyRaw;
     if (key == nullptr) continue;
@@ -31,7 +37,10 @@ void plotAll(TString pathFileIn, TString dirOut) {
     if (obj == nullptr) continue;
     if (!obj->IsA()->InheritsFrom("TH1")) continue;
     TH1 *hist = (TH1 *) key->ReadObj();
-    hist->Draw();
+    if (normalize) {
+      hist->Scale(1.0 / hist->Integral());
+    }
+    hist->Draw(optionDraw);
     gPad->Print(dirOut + seperatorPath + basenameFileIn + "_" + name + ".svg");
   }
   fileIn->Close();
