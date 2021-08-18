98d6164ae8fd9af877fd1318cbcdd861dcc804cb
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-08-19 05:45:56 +0800

    upgrade ROOT to the latest master commit to try to fix the Redefine segfault

--- lxplus_HTcondor/preselect/flake.nix
+++ lxplus_HTcondor/preselect/flake.nix
@@ -2,9 +2,20 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
   inputs.nixpkgs-root.url = "github:ShamrockLee/nixpkgs/root-6-25";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = inputs@{nixpkgs, nixpkgs-root, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (system: let
+  inputs.root-source.url = "github:root-project/root/master";
+  inputs.root-source.flake = false;
+  outputs = inputs@{nixpkgs, nixpkgs-root, flake-utils, root-source, ...}: flake-utils.lib.eachDefaultSystem (system: let
     pkgs = nixpkgs.legacyPackages.${system};
-    pkgs-root = nixpkgs-root.legacyPackages.${system};
+    pkgs-root = import nixpkgs-root {
+      inherit system;
+      overlays = [
+        (final: prev: {
+          root = prev.root.overrideAttrs (oldAttrs: {
+            src = root-source;
+          });
+        })
+      ];
+    };
   in{
     legacyPackages = pkgs;
     legacyPackages-root = pkgs-root;
