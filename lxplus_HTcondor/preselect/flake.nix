a703b975e2f14dd27ddc6fd77ecb4f6ad43434c3
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2021-08-20 15:28:11 +0800

    flake.nix: build with -Dimt=ON

--- lxplus_HTcondor/preselect/flake.nix
+++ lxplus_HTcondor/preselect/flake.nix
@@ -13,6 +13,17 @@
           root = prev.root.overrideAttrs (oldAttrs: {
             version = "2021-08-19";
             src = root-source;
+            cmakeFlags = map (oldFlag:
+              if oldFlag == "-Dimt=OFF" then "-Dimt=ON"
+              else if oldFlag == "-Dssl=OFF" then "-Dssl=ON"
+              # else if oldFlag == "-Dgfal=OFF" then "-Dgfal=ON"
+              # else if oldFlag == "-Dxrootd=OFF" then "-Dxrootd=ON"
+              else oldFlag
+            ) oldAttrs.cmakeFlags;
+            buildInputs = oldAttrs.buildInputs ++ (with pkgs-root; [
+              tbb # for implicit multithreading
+              openssl # for ssl support
+            ]);
           });
         })
       ];
