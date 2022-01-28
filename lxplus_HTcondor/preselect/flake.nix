3cfc2911f55a94887a69cd34974611d8bcbf0a25
Shamrock Lee <44064051+ShamrockLee@users.noreply.github.com>
2022-01-28 20:11:40 +0800

    flake.nix, flake.lock: use nixpkgs from nixos-21.11 to get package fixes (especially for pkgsStatic.libtiff)

--- lxplus_HTcondor/preselect/flake.nix
+++ lxplus_HTcondor/preselect/flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.root-source.url = "github:root-project/root/master";
   inputs.root-source.flake = false;
